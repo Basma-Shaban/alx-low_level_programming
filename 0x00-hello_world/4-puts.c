098f004534b48e01c09cab94fc764eba40981fe6
Basma-Shaban <shaabanbasma@gmail.com>
2023-08-09 23:46:08 -0700

    Basma

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,15 @@
-/**
- * main - Prints a sentence using puts
- * 
- * Return: 0 (Success)
-*/
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints with puts function
+ *
+ * Return: Always 0 (Success)
+*/
+
 int main(void)
-  {
-    puts("Programming is like building a multilingual puzzle\n");
-    return (0);
-  }
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
+}

919dd9ed0db814415ae2e8b766323c48ca827589
Basma-Shaban <shaabanbasma@gmail.com>
2023-08-09 23:21:58 -0700

    Basma

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - Prints a sentence using puts
  * 
@@ -7,7 +6,6 @@
  *
  * Return: 0 (Success)
 */
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");

5b5b7e4b7aaa137b47a71f3c0bbe87258eba9950
Basma-Shaban <shaabanbasma@gmail.com>
2023-08-11 11:11:34 -0700

    Basma

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -5,7 +5,7 @@
 /**
  * Main - Prints the last digit of a randomly generated number
  *
- * and whether it is greater than 5, less than 6, or 0.
+ * Description: and whether it is greater than 5, less than 6, or 0.
  *
  * Return: Always 0.
 */

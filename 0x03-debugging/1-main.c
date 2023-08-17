264d24442b6c2460e768e4bf19c4c5153187490f
Basma-Shaban <shaabanbasma@gmail.com>
2023-08-17 02:24:41 -0700

    Basma

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -2,17 +2,8 @@
 
 /**
  * main - causes an infinite loop
- *
- *      while (i < 10)
- *{
- *      putchar(i);
- *}
- *
- * Description: using printf function to print both "Infinite loop incoming :("
- *		and in the next line print "Infinite loop avoided! \o/"
- *
- * Return: Always 0 (success)
-*/
+ * Return: 0
+ */
 
 int main(void)
 {
@@ -20,12 +11,12 @@ int main(void)
 
 	printf("Infinite loop incoming :(\n");
 	i = 0;
-
-	while (i < 10)
-{
-	putchar(i);
-}
-
+/*
+* while (i < 10)
+* {
+* putchar(i);
+* }
+*/
 	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);

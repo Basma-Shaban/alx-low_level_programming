1d509f51760440da837ee65956dd32d9bf2a2f13
Basma-Shaban <shaabanbasma@gmail.com>
2023-08-20 22:55:31 -0700

    Basma

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+* rev_string - Reverses a string.
+* @s: string to reverse.
+*/
+
+void rev_string(char *s)
+{
+	int i, j;
+	char str[800];
+
+	i = j = 0;
+	while (s[i] != '\0')
+	{
+		str[i] = s[i];
+		i++;
+	}
+	i--;
+	while (i >= 0)
+	{
+		s[i] = str[j];
+		i--;
+		j++;
+	}
+	s[j++] = '\0';
+}

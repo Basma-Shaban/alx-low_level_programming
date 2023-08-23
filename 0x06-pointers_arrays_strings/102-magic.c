7554e3fb4be565d13e78fa745d3d0d44d29f3af7
Basma-Shaban <shaabanbasma@gmail.com>
2023-08-23 12:35:13 -0700

    Basma

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main: Entry Point
+ *
+ * Description: the code will print 98 in a new line
+ *
+ * Return: Always 0 (success)
+ */
+
 int main(void)
 {
 	int n;
@@ -16,8 +24,14 @@ int main(void)
    * - only one statement
    * - you are not allowed to code anything else than this line of code
    */
+	printf("n = %p\n", &n);
+	printf("a[0] = %p\n", &a[0]);
+	printf("a[1] = %p\n", &a[1]);
+	printf("a[2] = %p\n", &a[2]);
+	printf("p = %p\n", (p + 5));
+	printf("a[3] = %p\n", &a[3]);
 	*(p + 5) = 98;
 	/* ...so that this prints 98\n */
 	printf("a[2] = %d\n", a[2]);
 	return (0);
-}
+

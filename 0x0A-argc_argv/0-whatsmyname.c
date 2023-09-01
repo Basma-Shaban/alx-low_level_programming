1697b60d9d1d8ef9126f9878a37154ba18a93c8a
Basma-Shaban <shaabanbasma@gmail.com>
2023-09-01 05:45:32 -0700

    Basma

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -9,9 +9,13 @@
  * Return: Always o.
 */
 
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-	if (argc > 0)
-		printf("%s\n", argv[0]);
+	int i;
+
+	for (i = 0; i < argc; i++)
+{
+	printf("%s\n", argv[i]);
+}
 	return (0);
 }

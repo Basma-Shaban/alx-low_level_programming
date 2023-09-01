e7d1bed9d9269b3355aea7af5d45180f00777d8a
Basma-Shaban <shaabanbasma@gmail.com>
2023-09-01 05:36:53 -0700

    Basma

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - Prints the program name, followed by a new line.
@@ -8,12 +9,9 @@
  * Return: Always o.
 */
 
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-int i;
-for (i = 0; i < argc; i++)
-{
-printf("%s\n", argv[i]);
-}
-return (0);
+	if (argc > 0)
+		printf("%s\n", argv[0]);
+	return (0);
 }

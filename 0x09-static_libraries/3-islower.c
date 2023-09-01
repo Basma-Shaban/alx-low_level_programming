c7c0f69e92a4a0f25f94e9839c11b2dbf83e3bd1
Basma-Shaban <shaabanbasma@gmail.com>
2023-09-01 03:29:38 -0700

    Basma

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _islower - function to check if
+ *		character is lowercase
+ *
+ * @c: checks input of function
+ *
+ * Return: 1 if 'c' is lowercase
+ *		otherwise always 0 (success)
+*/
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar ('\n');
+}

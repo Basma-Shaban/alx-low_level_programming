e1527b8eedcab24a89f21efabb08aa5b4e498714
Basma-Shaban <shaabanbasma@gmail.com>
2023-09-11 10:54:21 -0700

    Basma

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,12 @@
+#ifndef _FUNCTION_LIKE_MACRO_
+#define _FUNCTION_LIKE_MACRO_
+
+/*
+ * File: 0-object_like_macro.h
+ * Desc: function-like macro ABS(x) that computes the
+ *       absolute value of a number x.
+ */
+
+#define ABS(x) ((x) < (0) ? ((x) * (-1)) : (x))
+
+#endif

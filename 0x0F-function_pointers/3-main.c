8924d605656c19085cca659b6348df88524cd990
Basma-Shaban <shaabanbasma@gmail.com>
2023-09-14 12:03:49 -0700

    Basma

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,32 +1,40 @@
 #include "3-calc.h"
-#include <stdio.h>
-#include <stdlib.h>
 
 /**
- * main - Prints the result of simple operations.
- * @argc: The number of arguments supplied to the program.
- * @argv: An array of pointers to the arguments.
+ * main - check the code for Holberton School students.
+ * @argc: argument count.
+ * @argv: argument vector.
+ *
  * Return: Always 0.
-*/
-
+ */
 int main(int argc, char *argv[])
 {
-int a, b, rsl;
-int (*f)(int, int);
-if (argc != 4)
-{
-printf("Error\n");
-exit(98);
-}
-a = atoi(argv[1]);
-b = atoi(argv[3]);
-f = get_op_func(argv[2]);
-if (f == NULL)
-{
-printf("Error\n");
-exit(99);
-}
-rsl = f(a, b);
-printf("%d\n", rsl);
-return (0);
+	int a, b;
+	int (*operation)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	if (argv[2][1])
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	operation = get_op_func(argv[2]);
+
+	if (operation == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+
+	printf("%d\n", operation(a, b));
+	return (0);
 }

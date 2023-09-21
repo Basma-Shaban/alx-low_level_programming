99286c1cf2f91b4391d0664a4c5a19eb6cbc4937
Basma-Shaban <shaabanbasma@gmail.com>
2023-09-21 11:29:52 -0700

    Basma

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/*
+ * File: 1-list_len.c.
+ * Author: Basma Shaban Ali
+ * email: shaabanbasma@gmail.com
+ */
+
+/**
+ * list_len - prints all the elements
+ *              of a "list_t" list.
+ * @h: pointer to the start of the list.
+ * Return: number of nodes in the list.
+ */
+
+size_t list_len(const list_t *h)
+{
+	const list_t *tmp;
+	unsigned int i;
+	tmp = h;
+	for (i = 0; tmp; i++)
+	tmp = tmp->next;
+	return (i);
+}
---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/*
+ * File: 4-free_list.c
+ * Author: Basma Shaban Ali
+ * email: shaabanbasma@gmail.com
+ */
+
+/**
+ * free_list - free a "list_t" list.
+ * @head: head of linked list.
+ */
+
+void free_list(list_t *head)
+{
+	list_t *tmp;
+	while (head)
+	{
+		tmp = head->next;
+		free(head->str);
+		free(head);
+		head = tmp;
+	}
+}

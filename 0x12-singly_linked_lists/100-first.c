2af6670c086e199e5c46a05c74fd05802ecd761b
Basma-Shaban <shaabanbasma@gmail.com>
2023-09-21 12:44:12 -0700

    Basma

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -5,20 +5,15 @@
  *
  * Auther: Basma Shaban Ali
  */
+void first(void) __attribute__ ((constructor));
 
 /**
- * bmain - Function executed before main.
- *
- * This function is executed before the main() function.
- * It can be used to initialize the kernel or to perform other tasks
- * that need to be done before the main application starts running.
- *
- * Return:
- *   None.
+ * first - prints a sentence before the main
+ * function is executed
  */
-void __attribute__ ((constructor)) bmain(void)
+void first(void)
 {
-	printf("You're beat! and yet, you must allow");
-	printf(",\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
 

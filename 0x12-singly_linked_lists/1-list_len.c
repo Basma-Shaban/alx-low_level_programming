610b50972d006dd7cb2506a68f97d3423340309a
Basma-Shaban <shaabanbasma@gmail.com>
2023-09-21 11:40:16 -0700

    Basma

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -17,6 +17,7 @@ size_t list_len(const list_t *h)
 {
 	const list_t *tmp;
 	unsigned int i;
+
 	tmp = h;
 
 	for (i = 0; tmp; i++)
--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -20,6 +20,7 @@ list_t *add_node_end(list_t **head, const char *str)
 	char *duplicate;
 	int len;
 	list_t *new, *last;
+
 	new = malloc(sizeof(list_t));
 
 	if (new == NULL)
